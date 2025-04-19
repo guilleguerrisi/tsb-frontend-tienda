678408404beb8ced1d1c9906b215ebf7b50f0a03
GuiGue <guilleguerrisi@gmail.com>
2025-04-19 19:12:54 -0300

    Corrijo URL del backend para producción

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
-    : 'http://192.168.100.70:5000';
+    : 'https://tsb-backend-tienda-production.up.railway.app';
 
 export default API_URL;
